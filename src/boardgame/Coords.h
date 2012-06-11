f73b0857e939d418060c6e6bccbfa1537a064ba8
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-06-11 23:36:19 +0200

    Reduce Coords to its base functionality and drop factory/caching

--- src/boardgame/Coords.h
+++ src/boardgame/Coords.h
@@ -13,59 +13,21 @@
 namespace boardgame {
 
     typedef unsigned char uint8_t; // Evil but i don't care ...
-    typedef unsigned short uint16_t; // This is evil, too
-
-    class CoordFactory;
 
     class Coords {
         public:
-            bool inValidRange(const uint8_t x, const uint8_t y) const;
-            Coords& valueOf(const uint8_t x, const uint8_t y) const;
-
-            uint8_t getX() const;
-            uint8_t getY() const;
-
-            CoordFactory& getFactory() const;
-
-            Coords& setX(const uint8_t x) const;
-            Coords& setY(const uint8_t y) const;
-
-        private:
-            friend class CoordFactory;
-
-            Coords(CoordFactory &factory, const uint8_t x, uint8_t y);
-            Coords(Coords &c);
+            Coords(const uint8_t x, const uint8_t y);
             ~Coords();
 
-            Coords& operator=(const Coords &c);
+            uint8_t getX() const;
+            Coords setX(const uint8_t x) const;
 
-            CoordFactory& factory;
-            const uint8_t x;
-            const uint8_t y;
-    };
+            uint8_t getY() const;
+            Coords setY(const uint8_t y) const;
 
-    class CoordFactory {
-        public:
-            CoordFactory(const uint8_t dimX, const uint8_t dimY);
-            CoordFactory(const CoordFactory &cf);
-            ~CoordFactory();
-
-            CoordFactory& operator=(const CoordFactory &cf);
-
-            bool inValidRange(const uint8_t x, const uint8_t y) const;
-            Coords& getCoords(const uint8_t x, const uint8_t y) const;
-
-            uint8_t getDimX() const;
-            uint8_t getDimY() const;
         private:
-
-            Coords* initCoords();
-
-            const uint8_t dimX;
-            const uint8_t dimY;
-
-            Coords * const coords;
-            size_t * const coordInstances;
+            /* const */ uint8_t x;
+            /* const */ uint8_t y;
     };
 
 }
