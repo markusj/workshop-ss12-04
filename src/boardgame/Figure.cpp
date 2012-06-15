a5eb11e14ee8c5def3904372b56a4129c4bcac93
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-06-15 03:26:41 +0200

    Add Figure implementation

--- src/boardgame/Figure.cpp
+++ src/boardgame/Figure.cpp
@@ -9,4 +9,34 @@
 
 namespace boardgame {
 
+    AbstractFigure::AbstractFigure(Player * const p) :
+        player(p),
+        board(NULL),
+        position(Coords(0, 0)) {
+    }
+
+    Player* AbstractFigure::getPlayer() const {
+        return player;
+    }
+
+    void AbstractFigure::setPlayer(Player &p) {
+        player = &p;
+    }
+
+    const Coords& AbstractFigure::getPosition() const {
+        return position;
+    }
+
+    void AbstractFigure::setPosition(const Coords& c) {
+        position = c;
+    }
+
+    Board* AbstractFigure::getBoard() const {
+        return board;
+    }
+
+    void AbstractFigure::setBoard(Board &b) {
+        board = &b;
+    }
+
 }
